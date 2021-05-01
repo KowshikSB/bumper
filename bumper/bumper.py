57a496f3a2e7a856d6079fc01c3de441ff5f9e50
KowshikSB <72295750+KowshikSB@users.noreply.github.com>
2021-05-01 10:55:41 +0530

    Update bumper.py

--- bumper/bumper.py
+++ bumper/bumper.py
@@ -1,3 +1,4 @@
+from discord.embeds import Embed
 from discord.ext import commands
 from discord import channel, utils
 import discord
@@ -27,6 +28,7 @@ class bumper(commands.Cog):
           rev='(https://disboard.org/review/create/799526257506254868)'
 
           if 'Bump done' in y: 
+              
               await channel.send(f'<:fox_wot:836972475828404255> *Drop a review* **[here]{rev}** if you want')
               await asyncio.sleep(7200)
               em=discord.Embed(title='Discord Bump Reminder!',description=x,color=0x2f3136)
@@ -34,6 +36,12 @@ class bumper(commands.Cog):
               await channel.send ('<a:capoo_work:825020992609976380> <@&825015601365778482>',embed=em)
         except IndexError:
           return None
-            
+    @commands.command()
+    async def test_bump(self,message):
+        guild=self.bot.get_guild(799526257506254868)
+        channel = guild.get_channel(826399186659442689)
+        rev='(https://disboard.org/review/create/799526257506254868)'
+        em=discord.Embed(description='<:fox_wot:836972475828404255> *Drop a review* **[here]{rev}** if you want',color=0x2f3136)
+        await channel.send(embed=em)
 def setup(bot):
     bot.add_cog(bumper(bot))
